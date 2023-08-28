c3d25721d431528efbd7cb5106db0d9ef833c314
LuYF-Lemon-love <3555028709@qq.com>
2023-08-28 15:09:41 +0800

    研究 PPO

--- guides/discrete_test_ppo.py
+++ guides/discrete_test_ppo.py
@@ -68,108 +68,101 @@ def get_args():
     args = parser.parse_known_args()[0]
     return args
 
-if __name__ == '__main__':
-
-    args=get_args()
-
-    env = gym.make(args.task)
-    args.state_shape = env.observation_space.shape or env.observation_space.n
-    args.action_shape = env.action_space.shape or env.action_space.n
-    if args.reward_threshold is None:
-        default_reward_threshold = {"CartPole-v0": 195}
-        args.reward_threshold = default_reward_threshold.get(
-            args.task, env.spec.reward_threshold
-        )
-    # train_envs = gym.make(args.task)
-    # you can also use tianshou.env.SubprocVectorEnv
-    train_envs = DummyVectorEnv(
-        [lambda: gym.make(args.task) for _ in range(args.training_num)]
+args=get_args()
+env = gym.make(args.task)
+args.state_shape = env.observation_space.shape or env.observation_space.n
+args.action_shape = env.action_space.shape or env.action_space.n
+if args.reward_threshold is None:
+    default_reward_threshold = {"CartPole-v0": 195}
+    args.reward_threshold = default_reward_threshold.get(
+        args.task, env.spec.reward_threshold
     )
-    # test_envs = gym.make(args.task)
-    test_envs = DummyVectorEnv(
-        [lambda: gym.make(args.task) for _ in range(args.test_num)]
+# train_envs = gym.make(args.task)
+# you can also use tianshou.env.SubprocVectorEnv
+train_envs = DummyVectorEnv(
+    [lambda: gym.make(args.task) for _ in range(args.training_num)]
+)
+# test_envs = gym.make(args.task)
+test_envs = DummyVectorEnv(
+    [lambda: gym.make(args.task) for _ in range(args.test_num)]
+)
+# seed
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+train_envs.seed(args.seed)
+test_envs.seed(args.seed)
+# model
+net = Net(args.state_shape, hidden_sizes=args.hidden_sizes, device=args.device)
+if torch.cuda.is_available():
+    actor = DataParallelNet(
+        Actor(net, args.action_shape, device=None).to(args.device)
     )
-    # seed
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    train_envs.seed(args.seed)
-    test_envs.seed(args.seed)
-    # model
-    net = Net(args.state_shape, hidden_sizes=args.hidden_sizes, device=args.device)
-    if torch.cuda.is_available():
-        actor = DataParallelNet(
-            Actor(net, args.action_shape, device=None).to(args.device)
-        )
-        critic = DataParallelNet(Critic(net, device=None).to(args.device))
-    else:
-        actor = Actor(net, args.action_shape, device=args.device).to(args.device)
-        critic = Critic(net, device=args.device).to(args.device)
-    actor_critic = ActorCritic(actor, critic)
-    # orthogonal initialization
-    for m in actor_critic.modules():
-        if isinstance(m, torch.nn.Linear):
-            torch.nn.init.orthogonal_(m.weight)
-            torch.nn.init.zeros_(m.bias)
-    optim = torch.optim.Adam(actor_critic.parameters(), lr=args.lr)
-    dist = torch.distributions.Categorical
-    policy = PPOPolicy(
-        actor,
-        critic,
-        optim,
-        dist,
-        action_scaling=isinstance(env.action_space, Box),
-        discount_factor=args.gamma,
-        max_grad_norm=args.max_grad_norm,
-        eps_clip=args.eps_clip,
-        vf_coef=args.vf_coef,
-        ent_coef=args.ent_coef,
-        gae_lambda=args.gae_lambda,
-        reward_normalization=args.rew_norm,
-        dual_clip=args.dual_clip,
-        value_clip=args.value_clip,
-        action_space=env.action_space,
-        deterministic_eval=True,
-        advantage_normalization=args.norm_adv,
-        recompute_advantage=args.recompute_adv
-    )
-    # collector
-    train_collector = Collector(
-        policy, train_envs, VectorReplayBuffer(args.buffer_size, len(train_envs))
-    )
-    test_collector = Collector(policy, test_envs)
-    # log
-    log_path = os.path.join(args.logdir, args.task, 'ppo')
-    writer = SummaryWriter(log_path)
-    logger = TensorboardLogger(writer)
-
-    def save_best_fn(policy):
-        torch.save(policy.state_dict(), os.path.join(log_path, 'policy.pth'))
-
-    def stop_fn(mean_rewards):
-        return mean_rewards >= args.reward_threshold
-
-    # trainer
-    result = OnpolicyTrainer(
-        policy=policy,
-        train_collector=train_collector,
-        test_collector=test_collector,
-        max_epoch=args.epoch,
-        step_per_epoch=args.step_per_epoch,
-        repeat_per_collect=args.repeat_per_collect,
-        episode_per_test=args.test_num,
-        batch_size=args.batch_size,
-        step_per_collect=args.step_per_collect,
-        stop_fn=stop_fn,
-        save_best_fn=save_best_fn,
-        logger=logger
-    ).run()
-    assert stop_fn(result['best_reward'])
-
-    pprint.pprint(result)
-    # Let's watch its performance!
-    env = gym.make(args.task)
-    policy.eval()
-    collector = Collector(policy, env)
-    result = collector.collect(n_episode=1, render=args.render)
-    rews, lens = result["rews"], result["lens"]
-    print(f"Final reward: {rews.mean()}, length: {lens.mean()}")
+    critic = DataParallelNet(Critic(net, device=None).to(args.device))
+else:
+    actor = Actor(net, args.action_shape, device=args.device).to(args.device)
+    critic = Critic(net, device=args.device).to(args.device)
+actor_critic = ActorCritic(actor, critic)
+# orthogonal initialization
+for m in actor_critic.modules():
+    if isinstance(m, torch.nn.Linear):
+        torch.nn.init.orthogonal_(m.weight)
+        torch.nn.init.zeros_(m.bias)
+optim = torch.optim.Adam(actor_critic.parameters(), lr=args.lr)
+dist = torch.distributions.Categorical
+policy = PPOPolicy(
+    actor,
+    critic,
+    optim,
+    dist,
+    action_scaling=isinstance(env.action_space, Box),
+    discount_factor=args.gamma,
+    max_grad_norm=args.max_grad_norm,
+    eps_clip=args.eps_clip,
+    vf_coef=args.vf_coef,
+    ent_coef=args.ent_coef,
+    gae_lambda=args.gae_lambda,
+    reward_normalization=args.rew_norm,
+    dual_clip=args.dual_clip,
+    value_clip=args.value_clip,
+    action_space=env.action_space,
+    deterministic_eval=True,
+    advantage_normalization=args.norm_adv,
+    recompute_advantage=args.recompute_adv
+)
+# collector
+train_collector = Collector(
+    policy, train_envs, VectorReplayBuffer(args.buffer_size, len(train_envs))
+)
+test_collector = Collector(policy, test_envs)
+# log
+log_path = os.path.join(args.logdir, args.task, 'ppo')
+writer = SummaryWriter(log_path)
+logger = TensorboardLogger(writer)
+def save_best_fn(policy):
+    torch.save(policy.state_dict(), os.path.join(log_path, 'policy.pth'))
+def stop_fn(mean_rewards):
+    return mean_rewards >= args.reward_threshold
+# trainer
+result = OnpolicyTrainer(
+    policy=policy,
+    train_collector=train_collector,
+    test_collector=test_collector,
+    max_epoch=args.epoch,
+    step_per_epoch=args.step_per_epoch,
+    repeat_per_collect=args.repeat_per_collect,
+    episode_per_test=args.test_num,
+    batch_size=args.batch_size,
+    step_per_collect=args.step_per_collect,
+    stop_fn=stop_fn,
+    save_best_fn=save_best_fn,
+    logger=logger
+).run()
+assert stop_fn(result['best_reward'])
+pprint.pprint(result)
+# Let's watch its performance!
+env = gym.make(args.task)
+policy.eval()
+collector = Collector(policy, env)
+result = collector.collect(n_episode=1, render=args.render)
+rews, lens = result["rews"], result["lens"]
+print(f"Final reward: {rews.mean()}, length: {lens.mean()}")
